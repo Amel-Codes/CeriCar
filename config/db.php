db6bd4b89fb1d856cb5402fdee882ff43130d96d
Amel-Codes <amelsekouane003@gmail.com>
2025-05-08 10:16:17 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,23 +1,10 @@
 <?php
 
-return [
-    
+return [ 
      //Pour accéder à la table des utilisateurs 
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=127.0.0.1;dbname=etd',
-    'username' => 'uapv2400316',
-    'password' => 'Y0Qa6w',
+    'username' => 'uapv-------',
+    'password' => '-------',
     'charset' => 'utf8',
-
-  /*
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
- */
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
